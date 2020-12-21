6d49bf5f10a0a6a0b2b490850ec913e7cb21d13f
DebarghaG <maildebargha@gmail.com>
2020-12-21 16:47:35 +0530

    NetworkX PageRank done

--- src/parser.py
+++ src/parser.py
@@ -1,25 +1,36 @@
 """
 Parser to read the files
 """
+import networkx as nx
+
 
 class Parser:
     def __init__(self, filename):
         self.filename = filename
+        self.G = nx.Graph()
 
     def read(self):
         file1 = open(self.filename, 'r')
         count = 0
-        while True:
+        #while True: if you have the RAM of a chad
+        while count<100000:
             count += 1
             line = file1.readline()
-
             if not line:
                 break
+            e = map(int,line.split())
+            self.G.add_edge(*e)
+
             #print("Line{}: {}".format(count, line.strip()))
 
         file1.close()
-        print("Read : " + str(count) + " Files")
+        print("Read : " + str(count) + " Lines")
+
+    def PageRank(self):
+        self.pr = nx.pagerank(self.G, alpha=0.85)
+        print(self.pr)
 
 if __name__ == '__main__':
     parse = Parser('../data/GoogleWeb.txt')
     parse.read()
+    parse.PageRank()
